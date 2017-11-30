7c96d2c690e5f3c63ed503e80b92fc38f118309c
Ryan O'Connor <charle692@gmail.com>
2017-11-30 00:07:01 -0500

    Removed unused code in settings.js

--- settings.js
+++ settings.js
@@ -4,19 +4,18 @@ import {
   StyleSheet,
   Text,
   View,
-  Image,
   TextInput
 } from 'react-native';
 
-const Spinner = require('react-native-spinkit');
-
 export default class Settings extends Component {
+
   constructor() {
     super();
     this.state = {
       loading: true
     }
   }
+
   render() {
     return (
       <View style={styles.container}>
@@ -27,25 +26,9 @@ export default class Settings extends Component {
 }
 
 const styles = StyleSheet.create({
-  hockeyLight: {
-    height: 120,
-    width: 240
-  },
   container: {
-    paddingTop: 50,
+    paddingTop: 10,
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#999',
   },
-  title: {
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
-    marginTop: 20,
-    color: '#FEFFFE',
-  },
-  spinner: {
-    color: '#ffffff',
-    marginTop: 20,
-  }
 });
